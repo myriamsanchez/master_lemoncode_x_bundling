9e51ed8c45ceaba8a455b621c527a8ba840d0929
nekoinverse <myriamsanchez@users.noreply.github.com>
2021-09-19 05:03:03 +0200

    -added better styles for the logo and text -changed some html structure -commented unneeded code

--- webpack_bundling/src/index.ts
+++ webpack_bundling/src/index.ts
@@ -1,15 +1,15 @@
-import * as averageService from './averageService';
-import logoImg from './content/cat_ball.png';
+// import * as averageService from './averageService';
+import logoImg from './content/cat_sit.png';
 
-const scores = [90, 75, 60, 99, 94, 30];
-const averageScore = averageService.getAvg(scores);
-const totalScore = averageService.getTotalScore(scores);
+// const scores = [90, 75, 60, 99, 94, 30];
+// const averageScore = averageService.getAvg(scores);
+// const totalScore = averageService.getTotalScore(scores);
 
-const messageToDisplayAvg = `average score ${averageScore}`;
-const messageToDisplayTotal = `total score ${totalScore}`;
+// const messageToDisplayAvg = `average score ${averageScore}`;
+// const messageToDisplayTotal = `total score ${totalScore}`;
 
-document.write(messageToDisplayAvg);
-document.write(messageToDisplayTotal);
+// document.write(messageToDisplayAvg);
+// document.write(messageToDisplayTotal);
 
 const img = document.createElement('img');
 img.src = logoImg;
